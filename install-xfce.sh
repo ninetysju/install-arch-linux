7be485d2412219ad9959e31e0d2864354a81b497
Max Dahl <dax.mahl@gmail.com>
2021-03-28 16:52:48 +0200

    add whiskermenu

--- install-xfce.sh
+++ install-xfce.sh
@@ -3,12 +3,12 @@
 sudo pacman -S --noconfirm \
   xorg-server \
   xfce4 lightdm lightdm-gtk-greeter \
-  xfce4-taskmanager xfce4-notifyd xfce4-screensaver xfce4-screenshooter \
+  xfce4-whiskermenu-plugin xfce4-taskmanager xfce4-notifyd xfce4-screensaver xfce4-screenshooter \
   xfce4-pulseaudio-plugin pavucontrol pulseaudio \
   nm-connection-editor network-manager-applet \
   gvfs ntfs-3g \
-  thunar-archive-plugin \
-  mousepad xarchiver ristretto qpdfview \
+  thunar-archive-plugin xarchiver \
+  mousepad ristretto qpdfview \
   ufw gufw \
   ffmpeg
 
